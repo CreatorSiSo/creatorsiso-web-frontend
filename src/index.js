114f682785d263e3879b3bb8b29fbaec91a68009
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-02-05 22:36:42 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import $ from 'jquery';
-import Popper from '../node_modules/@popperjs';
+import Popper from '@popperjs';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 ReactDOM.render(
