be49cb43735a7255fa9854e39f6f791b4ed292f2
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-02-06 08:13:29 +0100

    header h1->h2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header = ({bg_color, text_color, bs_style_Text, bs_style_small_Text, text, small_Text }) => {
     return (
         <header style={{color: text_color, backgroundColor: bg_color}} className='text-center'>
-            <h1 className={bs_style_Text}>{text}</h1>
+            <h2 className={bs_style_Text}>{text}</h2>
             <p className={bs_style_small_Text}>{small_Text}</p>
         </header>
     )
