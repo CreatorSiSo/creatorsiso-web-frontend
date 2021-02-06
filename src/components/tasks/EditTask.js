4f6eb6c33ab3ef0108580f6a5b73fa23c11ce82a
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-02-06 07:14:46 +0100

    Create EditTask.js

---
+++ src/components/tasks/EditTask.js
@@ -0,0 +1,93 @@
+import { BsCheck, BsX, BsChevronDown } from '../../../node_modules/react-icons/bs';
+import { FiX, FiCheck, FiEdit, FiCopy, FiXSquare } from '../../../node_modules/react-icons/fi';
+
+import Badge from '../Badge'
+import ButtonNestedDropdown from '../button/ButtonNestedDropdown'
+
+const bgDanger = 'rgba(231, 76, 60, .3)'
+const bgHover = 'rgba(231, 76, 60, .5)'
+const bgSuccess = 'rgba(0, 188, 140, .3)'
+const bgSuccessHover = 'rgba(0, 188, 140, .5)'
+
+const Task = ({ task, onClick, onToggle, onDelete, onDuplicate }) => {
+    return (
+        <div className={`card mb-2 ${[task.isDone ? 'border-success' : 'border-danger']}`}>
+            <div className='card-header pb-0 px-3'>
+                <div className='d-flex justify-content-between align-items-topline'>
+                    <input className='mb-0 mr-3' value={task.title}></input>
+                    {/* <h5 className='mb-0 mr-3'>{task.title}</h5> */}
+                    <div>
+                        {task.isDone ?
+                            <ButtonNestedDropdown   bs_style_button='border-success btn btn-outline-success badge'
+                                                    bs_style_dropdown='border-success btn btn-outline-success badge rounded-right'
+                                                    bs_style_dropdown_menu= 'dropdown-menu dropdown-menu-right border-success'
+                                                    bs_style_dropdown_item= 'dropdown-item d-flex align-items-center'
+
+                                                    bg_color={bgSuccess}
+                                                    
+                                                    button_text='Done'
+                                                    dropdown_item1_text='Edit Task'
+                                                    dropdown_item2_text='Delete Task'
+                                                    dropdown_item3_text='Duplicate Task'
+
+                                                    button_icon={<FiCheck/>}
+                                                    dropdown_icon={<BsChevronDown/>}
+                                                    dropdown_item1_icon={<FiEdit className='mr-3'/>}
+                                                    dropdown_item2_icon={<FiXSquare className='mr-3'/>}
+                                                    dropdown_item3_icon={<FiCopy className='mr-3'/>}
+
+                                                    isCustomDropdownMenuStyle= {false}
+                                                    isCustomDropdownItemStyle= {false}
+
+                                                    onClick={onClick}
+                                                    onToggle={onToggle}
+                                                    onDelete={onDelete}
+                                                    onDuplicate={onDuplicate}
+                                                    task={task}
+                            />
+                        :
+                            <ButtonNestedDropdown   bs_style_button='border-danger btn btn-outline-danger badge'
+                                                    bs_style_dropdown='border-danger btn btn-outline-danger badge rounded-right'
+                                                    bs_style_dropdown_menu= 'dropdown-menu dropdown-menu-right border-danger'
+                                                    bs_style_dropdown_item= 'dropdown-item d-flex align-items-center'
+
+                                                    bg_color={bgDanger}
+                                                    
+                                                    button_text='Not Done'
+                                                    dropdown_item1_text='Edit Task'
+                                                    dropdown_item2_text='Delete Task'
+                                                    dropdown_item3_text='Duplicate Task'
+
+                                                    button_icon={<FiX/>}
+                                                    dropdown_icon={<BsChevronDown/>}
+                                                    dropdown_item1_icon={<FiEdit className='mr-3'/>}
+                                                    dropdown_item2_icon={<FiXSquare className='mr-3'/>}
+                                                    dropdown_item3_icon={<FiCopy className='mr-3'/>}
+
+                                                    isCustomDropdownMenuStyle= {false}
+                                                    isCustomDropdownItemStyle= {false}
+
+                                                    onClick={onClick}
+                                                    onToggle={onToggle}
+                                                    onDelete={onDelete}
+                                                    onDuplicate={onDuplicate}
+                                                    task={task}
+                        />}
+                    </div>
+                </div>
+            <div>
+                <p className='mt-2 ml-1 user-select-none'>
+                    <Badge bs_style="badge badge-danger mr-1" text={task.tagFE}/>
+                    <Badge bs_style="badge badge-warning mr-1" text={task.tagBE}/>
+                    <Badge bs_style="badge badge-info mr-1" text={task.tagFS}/>
+                    <Badge bs_style="badge btn border-secondary mx-1" text={task.tagCustom}/>
+                </p>
+            </div>
+            <div className='card-body'>
+            <textarea rows='10' columns='0' value={task.text}></textarea>
+            </div>
+        </div>
+        </div>
+    )}
+
+export default Task
