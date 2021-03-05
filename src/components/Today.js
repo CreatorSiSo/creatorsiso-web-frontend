bfa00635a85a1643b40d98c2b9a8dd28220b3932
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-03-05 10:16:26 +0100

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,8 +1,10 @@
 let today = new Date();
+let minute = today.getMinutes()
+let hour = today.getHours();
 let day = String(today.getDate()).padStart(2, '0');
 let month = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 let year = today.getFullYear();
 
-const Today = day + '/' + month + '/' + year;
+const Today = day + '/' + month + '/' + year + '_' + hour + ':' + minute;
 
 export default Today
