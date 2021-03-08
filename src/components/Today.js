2f29a5c7ae91d68f1295433fc3d21fc8b07a7523
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2021-03-08 16:36:12 +0100

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,6 +1,6 @@
 let today   = new Date();
-let minute  = (today.getMinutes() < 0) ? '0' + today.getMinutes() : today.getMinutes();
-let hour    = today.getHours();
+let minute  = (today.getMinutes() < 9) ? '0' + today.getMinutes() : today.getMinutes();
+let hour    = (today.getHours() < 9) ? '0' + today.getHours() : today.getHours();
 let day     = String(today.getDate()).padStart(2, '0');
 let month   = String(today.getMonth() + 1).padStart(2, '0'); //January is 0
 let year    = today.getFullYear();
